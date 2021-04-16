a87a55cb51259966c69cab94936fe2b123a1f3c9
OhGyong <oky7143@naver.com>
2021-04-16 18:48:56 +0900

    nav_page_switch

---
+++ app/src/main/java/com/example/Idle_Project/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.Idle_Project
+
+import android.os.Bundle
+import android.view.Menu
+import android.widget.TextView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    //private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+        val navHome: TextView = findViewById(R.id.nav_idea_board);
+        navHome.setOnClickListener {
+            findNavController(R.id.nav_host_fragment).navigate(R.id.nav_idea_board, Bundle().apply {
+
+            })
+
+        }
+
+        /*
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+                R.id.nav_home,
+                R.id.nav_idea_board,
+                R.id.nav_anno_board
+        ), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    */
+
+    }
+/*
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+   */
+}
---
+++ app/src/main/java/com/example/Idle_Project/ui/idea_board/Idea_Board_Fragment.kt
@@ -0,0 +1,37 @@
+package com.example.Idle_Project.ui.idea_board
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.Idle_Project.R
+
+class Idea_Board_Fragment : Fragment() {
+
+    private lateinit var idea_board_model: GalleryViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        idea_board_model =
+                ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_idea_board, container, false)
+        val textView: TextView = root.findViewById(R.id.text_gallery)
+        idea_board_model.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
