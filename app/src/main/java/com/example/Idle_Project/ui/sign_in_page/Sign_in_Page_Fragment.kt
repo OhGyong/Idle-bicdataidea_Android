c41a22992c12d151eecfa2e6ea6c2caf7f1e5a29
OhGyong <oky7143@naver.com>
2021-04-23 18:39:39 +0900

    회원가입이용약관

--- app/src/main/java/com/example/Idle_Project/ui/sign_in_page/Sign_in_Page_Fragment.kt
+++ app/src/main/java/com/example/Idle_Project/ui/sign_in_page/Sign_in_Page_Fragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import com.example.Idle_Project.R
 
 class Sign_in_Page_Fragment : Fragment() {
@@ -15,7 +17,13 @@ class Sign_in_Page_Fragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val root = inflater.inflate(R.layout.fragment_sign_in_page, container, false)
+        var root = inflater.inflate(R.layout.fragment_sign_in_page, container, false)
+
+        val sign_up_bt: Button = root.findViewById(R.id.sign_up_go_bt)
+        sign_up_bt.setOnClickListener {
+            println("111111")
+        }
+
         return root
     }
 }
---
+++ app/src/main/java/com/example/Idle_Project/ui/sign_up_page/Sign_up_Agree_Fragment.kt
@@ -0,0 +1,20 @@
+package com.example.Idle_Project.ui.sign_up_page
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.Idle_Project.R
+
+class Sign_up_Agree_Fragement : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_sign_up_agree, container, false)
+        return root
+    }
+}
