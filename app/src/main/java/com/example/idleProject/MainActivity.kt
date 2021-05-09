01056901a38fd5e0436884090d88c37f643c2963
OhGyong <oky7143@naver.com>
2021-05-09 22:17:52 +0900

    Splash 및 AppIcon 변경

--- app/src/main/java/com/example/idleProject/MainActivity.kt
+++ app/src/main/java/com/example/idleProject/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.idleProject
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
---
+++ app/src/main/java/com/example/idleProject/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.idleProject
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import java.lang.Exception
+
+class SplashActivity : Activity() {
+
+    val DURATION:Long = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, DURATION)
+
+//        try {
+//            Thread.sleep(2000);
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }catch (e:Exception){
+//            return;
+//        }
+    }
+}
+
