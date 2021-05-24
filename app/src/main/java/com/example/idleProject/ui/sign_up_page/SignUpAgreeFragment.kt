fa0b48a65fd3ff8728ce931fd9e1a4c75e975fb4
OhGyong <oky7143@naver.com>
2021-05-25 01:08:28 +0900

    회원가입 이용약관 체크박스 처리중

--- app/src/main/java/com/example/idleProject/ui/sign_up_page/SignUpAgreeFragment.kt
+++ app/src/main/java/com/example/idleProject/ui/sign_up_page/SignUpAgreeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.idleProject.ui.sign_up_page
 
+import android.content.Context
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
@@ -7,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.idleProject.R
 
@@ -20,14 +23,25 @@ class SignUpAgreeFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_sign_up_agree, container, false)
 
-        //스크롤 처리 때문에 추가했는데 없어도 돌아감
-        var textView: TextView = root.findViewById(R.id.textview_signup1)
-        textView.setMovementMethod(ScrollingMovementMethod())
+        val checkBox1 : CheckBox = root.findViewById(R.id.sign_up_check1) // 체크박스 필수1
+        val checkBox2 : CheckBox = root.findViewById(R.id.sign_up_check2) // 체크박스 필수2
+        val checkBox3 : CheckBox = root.findViewById(R.id.sign_up_check3) // 체크박스 선택
+        val signUpAgreeBt: Button = root.findViewById(R.id.sign_up_agree_bt) // 확인 버튼
+        Toast.makeText(context, "필수 항목들을 체크하시기 바랍니다.", Toast.LENGTH_SHORT).show()
 
-        var signUpAgreeBt: Button = root.findViewById(R.id.sign_up_agree_bt)
+        // 확인 클릭시 회원가입 페이지로 이동
         signUpAgreeBt.setOnClickListener{
-            findNavController().navigate(R.id.nav_sign_up_page)
+
+            // 필수 요소들이 둘 다 체크되었을 경우 회원가입 페이지로 이동
+            if(checkBox1.isChecked && checkBox2.isChecked){
+                findNavController().navigate(R.id.nav_sign_up_page)
+            }
+            // 필수 요소들을 체크하지 않았을 경우 toast로 필수 요소 체크하라고 알림
+            else{
+                Toast.makeText(context, "필수 항목들을 체크하시기 바랍니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+
         return root
     }
 }
